8b886c609fe18d9ea503cf1c35514cb09afe080e
c1boo <mehmetali.cibo@gmail.com>
2023-02-27 21:40:46 +0100

    Added lasers and projectile logic to Player.cs

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -8,14 +8,24 @@ public class Player : MonoBehaviour {
     //Configuration Paramteres
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 0.5f;
+    [SerializeField] GameObject laserBeam;
+    [SerializeField] float projectileSpeed = 20f;
+    [SerializeField] float projectileFiringPeriod = 1f;
 
-    //State vars
     float minX, maxX;
     float minY, maxY;
+
+    Coroutine firingCoroutine;
 
     // Start is called before the first frame update
     void Start() {
         SetUpMovementBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Move();
+        Fire();
     }
 
     private void SetUpMovementBoundaries() {
@@ -28,11 +38,6 @@ public class Player : MonoBehaviour {
         maxY = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
-    // Update is called once per frame
-    void Update() {
-        Move();
-    }
-
     private void Move() {
         float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
@@ -41,5 +46,22 @@ public class Player : MonoBehaviour {
         float newPosY = Mathf.Clamp(transform.position.y + deltaY, minY, maxY);
 
         transform.position = new Vector2(newPosX, newPosY);
+    }
+
+    private void Fire() {
+        if (Input.GetButtonDown("Fire1")) {
+            firingCoroutine = StartCoroutine(FireContinuosly());
+        }
+        if (Input.GetButtonUp("Fire1")) {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    private IEnumerator FireContinuosly() {
+        while (true) {
+            GameObject laser = Instantiate(laserBeam, transform.position, Quaternion.identity);
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
     }
 }
